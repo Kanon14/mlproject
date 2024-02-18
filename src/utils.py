44b195df42933152ab9e0ac1e7899406ac764d62
Kanon14 <geochua144@gmail.com>
2024-02-18 22:04:37 +0800

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -20,26 +21,32 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
         
-        for name, model in models.items():
-            model.fit(X_train, y_train) # Train model
-            
-            # Make predictions
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
             y_train_pred = model.predict(X_train)
+
             y_test_pred = model.predict(X_test)
-            
-            # Predict R^2 score
+
             train_model_score = r2_score(y_train, y_train_pred)
+
             test_model_score = r2_score(y_test, y_test_pred)
-            
-            report[name] = {
-                'Train Score': train_model_score,
-                'Test Score': test_model_score
-            }
-            
+
+            report[list(models.keys())[i]] = test_model_score
+
         return report
     
     except Exception as e:
